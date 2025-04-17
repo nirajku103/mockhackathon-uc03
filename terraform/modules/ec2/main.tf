f14d31e0b54687c901a8de1a999e61c0641ed2a6
nirajku103 <nirajnetjob.hojai@gmail.com>
2025-04-17 09:04:57 +0000

    changing ami

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = element(var.subnets, count.index)
-  security_groups = [aws_security_group.web_sg.id]
+  security_groups = [aws_security_group.web-sg.id]
 
   user_data = file("${path.module}/user-data.sh")
 }
